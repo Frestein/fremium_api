de527c7cd0931145e683f18f8fb30c8982332441
Frestein <Frestein@outlook.com>
2024-03-18 05:16:06 +0500

    FRMA-1

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from typing import Union
-
 from fastapi import FastAPI
+
+from routers.posts import router as posts_router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(
+    router=posts_router,
+    prefix='/posts',
+)
---
+++ routers/posts.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/")
+def read_root():
+    return [
+        {
+            "id": 1,
+            "title": "Test title",
+            "description": "Test post description",
+        },
+        {
+            "id": 2,
+            "title": "Test title",
+            "description": "Test post description",
+        }
+    ]
+
+
+@router.get("/{post_id}")
+def read_post(post_id: int):
+    return {
+        "post_id": post_id,
+        "title": "Test title",
+        "description": "Test post description",
+    }
